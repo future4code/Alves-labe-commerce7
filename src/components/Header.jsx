8baa60fc6437f36963022db6fe0ad44d63e1caa5
Nicole Prim <nicolepprim@gmail.com>
2022-06-17 16:31:53 -0300

    novos componentes

---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import { AiOutlineShoppingCart } from 'react-icons/ai'
+
+export default class Header extends Component {
+    state = {
+        shopping: false
+    }
+
+    showCart = () => {
+        this.state.shopping = !this.state.shopping
+        this.setState({
+            shopping: this.state.shopping
+        })
+        console.log('oi')
+    }
+    render() {
+        let componentCart
+        if (this.state.shopping) {
+            componentCart = <Cart onClick={this.props.onClick} items={this.props.filterCar} totalCar={this.props.totalCar} />
+        }
+        return (
+            <div>
+
+                <button onClick={this.showCart}><AiOutlineShoppingCart /></button>
+
+                {componentCart}
+            </div>
+        )
+    }
+}
---
+++ src/components/ItemsCart.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class ItemsCart extends Component {
+  render() {
+    return (
+      <div>ItemsCart</div>
+    )
+  }
+}
